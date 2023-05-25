18147755e2c39fac1e7b2151b7732580c1185ccb
MatheusNachtigall <matheusgnachtigall@gmail.com>
2023-05-25 17:00:27 -0300

    update nest package

---
+++ api-com-nest/src/config/data-source.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+dotenv.config({ path: `${process.env.NODE_ENV ?? ''}.env` });
+
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+import { Video, Watcher, Category } from './entities';
+
+const port = process.env.POSTGRES_PORT as unknown as number | undefined;
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: port,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  // synchronize: false,
+  // entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+});
